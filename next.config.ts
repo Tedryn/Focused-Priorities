5c62e96c4e6aae986b3ca5c06f5ab6f9feedc7a3
Jeffrey Andersen <jeffrey@photography.com>
2026-08-08 19:44:35 -0500

    Allow R2 custom image domain

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,22 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'pub-442b55c6007744d585a90abfffacf60f.r2.dev',
+        protocol: "https",
+        hostname: "pub-442b55c6007744d585a90abfffacf60f.r2.dev",
+      },
+      {
+        protocol: "https",
+        hostname: "images.focusedpriorities.com",
       },
     ],
   },
 };
 
 export default nextConfig;
-import('@opennextjs/cloudflare').then(m => m.initOpenNextCloudflareForDev());
+
+import("@opennextjs/cloudflare").then((m) =>
+  m.initOpenNextCloudflareForDev()
+);
